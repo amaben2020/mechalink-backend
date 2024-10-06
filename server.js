2f2457b6e01442178515b9d4ef4c7d6fc04caf32
amaben2020 <amaben202phantom@gmail.com>
2024-10-06 09:45:28 +0100

    setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 200,
+    message: 'hi',
+  });
+});
+
+app.listen(8080, () => {
+  console.log('server is running');
+});

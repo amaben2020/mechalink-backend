15ee7c7a1f4d89c48e8859fda78e61556d1054c0
amaben2020 <amaben202phantom@gmail.com>
2024-10-11 05:26:18 -0500

    addded

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/api/home', (req, res) => {
 
 app.listen(`${process.env.PORT} || 5000`, () => {
   console.log(
-    `server is running fine... ${process.env.POSTGRES_URL} ===> ${process.env.PORT}`
+    `server is running fine ${process.env.POSTGRES_URL} ===> ${process.env.PORT}`
   );
 });

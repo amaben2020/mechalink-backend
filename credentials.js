806f71b987b240e8aa79cb7032b065ab50d1a0bf
amaben2020 <amaben202phantom@gmail.com>
2024-10-13 13:38:45 -0500

    sqs service

--- credentials.js
+++ credentials.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const configObject = {
   region: 'us-east-1',
+  // useQueueUrlAsEndpoint: false,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.SECRET_KEY,
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const { SQSClient, SendMessageCommand } = require('@aws-sdk/client-sqs');
+const {
+  SQSClient,
+  SendMessageCommand,
+  ReceiveMessageCommand,
+  DeleteMessageCommand,
+} = require('@aws-sdk/client-sqs');
 const { configObject } = '/credentials.js';
 
 // move to SQS factory
-const sqsClient = new SQSClient(configObject);
-const queueUrl = process.env.AWS_QUEUE_URL;
+const sqsClient = new SQSClient({
+  region: 'us-east-1',
+  // useQueueUrlAsEndpoint: false,
+  // endpoint: 'https://sqs.us-east-1.amazonaws.com',
+
+  // credentials: {
+  //   accessKeyId: process.env.AWS_ACCESS_KEY,
+  //   secretAccessKey: process.env.SECRET_KEY,
+  // },
+});
+
+const queueUrl =
+  'https://sqs.us-east-1.amazonaws.com/430118838661/JobNotificationQueue';
+
+const deleteMessageFromQueue = async (ReceiptHandle) => {
+  try {
+    const data = await sqsClient.send(
+      new DeleteMessageCommand({
+        QueueUrl: queueUrl,
+        ReceiptHandle,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // message to be sent
 const sendMessageToQueue = async (body) => {
@@ -18,15 +47,44 @@ const sendMessageToQueue = async (body) => {
         OrderId: { DataType: 'String', StringValue: '4421x' },
       },
     });
+    console.log(command);
     const result = await sqsClient.send(command);
     console.log(result);
-  } catch (error) {}
+    // once done processing, delete the message from the queue
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-sendMessageToQueue({
-  title: 'Job created',
-  description: 'Job should be consumed by firebase',
-});
+// sendMessageToQueue(
+//   JSON.stringify({
+//     title: 'Job created 003',
+//     description: 'Do anything with this data',
+//   })
+// );
+
+// consumes the
+const pollMessages = async () => {
+  try {
+    const command = new ReceiveMessageCommand({
+      MaxNumberOfMessages: 10,
+      QueueUrl: process.env.AWS_QUEUE_URL,
+      WaitTimeSeconds: 5,
+      MessageAttributes: ['All'],
+    });
+    const result = await sqsClient.send(command);
+    console.log(result.Messages[0]);
+    // after sending to firebase, delete the message
+
+    await deleteMessageFromQueue(result.Messages[0].ReceiptHandle);
+
+    console.log('Message deleted', result.Messages[0].Body);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+pollMessages();
 
 app.get('/api/home', (req, res) => {
   res.json({

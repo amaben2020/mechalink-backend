14ff3ab57b6b7cd1e561e614634d8e422f0755d7
amaben2020 <amaben202phantom@gmail.com>
2024-10-18 12:27:32 -0500

    replacing hardcoded values with env vars rather than hardcoded

--- drizzle.config.js
+++ drizzle.config.js
@@ -9,9 +9,8 @@ export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
     url:
-      // process.env.NODE_ENV === 'development'
-      //   ? process.env.DATABASE_URL_DEV
-      //   : process.env.DATABASE_URL_PROD,
-      'postgresql://mechalink_owner:gcY6DhXvK0Qx@ep-holy-meadow-a5vnosnq.us-east-2.aws.neon.tech/mechalink?sslmode=require',
+      process.env.NODE_ENV === 'development'
+        ? process.env.DATABASE_URL_DEV
+        : process.env.DATABASE_URL_PROD,
   },
 });
--- src/migrate.js
+++ src/migrate.js
@@ -1,31 +1,3 @@
-// import { drizzle } from 'drizzle-orm/neon-http';
-// import { migrate } from 'drizzle-orm/neon-http/migrator';
-// import { neon } from '@neondatabase/serverless';
-
-// const sql = neon(
-//   'postgresql://mechalink_owner:gcY6DhXvK0Qx@ep-holy-meadow-a5vnosnq.us-east-2.aws.neon.tech/mechalink?sslmode=require'
-// );
-
-// const db = drizzle(sql);
-
-// const main = async () => {
-//   console.log('Starting Migration....');
-//   try {
-//     await migrate(db, {
-//       migrationsFolder: '/migrations',
-//     });
-
-//     console.log('Migration successful');
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-// };
-
-// main();
-
-// src/migrate.ts
-
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { migrate } from 'drizzle-orm/neon-http/migrator';
@@ -34,7 +6,9 @@ import { config } from 'dotenv';
 config({ path: '.env' });
 
 const sql = neon(
-  'postgresql://mechalink_owner:gcY6DhXvK0Qx@ep-holy-meadow-a5vnosnq.us-east-2.aws.neon.tech/mechalink?sslmode=require'
+  process.env.NODE_ENV === 'development'
+    ? process.env.DATABASE_URL_DEV
+    : process.env.DATABASE_URL_PROD
 );
 const db = drizzle(sql);
 

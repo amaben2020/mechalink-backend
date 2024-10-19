911b4c9e6b64c054cc35370a10298ba61cdd8e8c
amaben2020 <amaben202phantom@gmail.com>
2024-10-19 12:22:55 -0500

    fixed schema

--- src/schema.js
+++ src/schema.js
@@ -2,4 +2,4 @@ export { usersTable } from './schema/user.js';
 export { availability } from './schema/availability.js';
 export { jobs as jobSchema } from './schema/job.js';
 export { jobRequests as jobRequestSchema } from './schema/jobRequest.js';
-export { mechanics as mechanicSchema } from './schema/mechanic';
+export { mechanics as mechanicSchema } from './schema/mechanic.ts';

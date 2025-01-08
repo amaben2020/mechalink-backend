224f26a4a457ff6f4d5dbe6a54606087d345a9fc
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-01-08 11:19:27 +0100

    updating signin

--- controller/auth/auth.signIn.ts
+++ controller/auth/auth.signIn.ts
@@ -17,13 +17,13 @@ export const signin = async (req: express.Request, res: express.Response) => {
     const { email, password } = signInSchema.parse(req.body);
     console.log(email);
 
-    const userHasRegistered = await db
+    const [userHasRegistered = undefined] = await db
       .select()
       .from(usersTable)
       .where(eq(usersTable.email, email))
       .execute();
 
-    if (userHasRegistered[0].email !== email) {
+    if (userHasRegistered?.email !== email) {
       res.status(403).send(`User with ${email} already exists`);
       throw new MechalinkAlreadyExists(`User with ${email} already exists`);
     }
@@ -40,9 +40,20 @@ export const signin = async (req: express.Request, res: express.Response) => {
     );
 
     if (userData?.user.uid) {
-      res
-        .status(200)
-        .json({ user: userData?.user, role: userHasRegistered[0].role });
+      res.status(200).json({
+        user: userData?.user,
+        role: userHasRegistered?.role,
+        fullName:
+          userHasRegistered?.firstName + ' ' + userHasRegistered?.lastName,
+        phoneNumber: userHasRegistered?.phone,
+        addressOne: userHasRegistered?.addressOne,
+        addressTwo: userHasRegistered?.addressTwo,
+        city: userHasRegistered?.city,
+        state: userHasRegistered?.state,
+        username: userHasRegistered?.username,
+        zip: userHasRegistered?.zip,
+        country: userHasRegistered?.country,
+      });
     } else {
       res.status(401).json({ message: 'Email or password is wrong' });
     }

403e6aa092fa882476c14793ecea57cb2caa333b
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-01-16 14:27:10 +0100

    adding the get mechanic by user id endpoint

--- controller/mechanics/mechanic.get.ts
+++ controller/mechanics/mechanic.get.ts
@@ -1,6 +1,7 @@
+import { eq } from 'drizzle-orm';
 import express from 'express';
 import { db } from 'src/db.ts';
-import { mechanicSchema } from 'src/schema.ts';
+import { mechanicSchema, usersTable } from 'src/schema.ts';
 
 export const mechanicsGetController = async (
   _: express.Request,
@@ -8,6 +9,24 @@ export const mechanicsGetController = async (
 ) => {
   try {
     const mechanic = await db.select().from(mechanicSchema);
+
+    res.json(mechanic);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const mechanicsByUserIdGetController = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { userId } = req.params;
+    const mechanic = await db
+      .select()
+      .from(mechanicSchema)
+      .where(eq(mechanicSchema.userId, Number(userId)))
+      .leftJoin(usersTable, eq(usersTable.id, mechanicSchema.id));
 
     res.json(mechanic);
   } catch (error) {
